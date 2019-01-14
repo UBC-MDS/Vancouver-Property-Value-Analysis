68fa658f10803dd9ff155ff631708f7318c6c82d
Ian Flores Siaca <iflores.siaca@gmail.com>
2019-01-14 10:01:38 -0800

    Uploading Shiny App Skeleton

---
+++ vancouver_property/ui.R
@@ -0,0 +1,43 @@
+library(shiny)
+library(shinythemes)
+
+shinyUI(
+    navbarPage(theme = shinytheme('yeti'),
+               "Vancouver Property Analysis App",
+        tabPanel('Home', icon = icon('home'),
+                 fluidPage(
+                     fluidRow(
+                         h1(class = 'text-center', 'Vancouver Property Analysis App'),
+                         column(2, 
+                                h3(class = 'text-center', 'Here goes one dropdown'),
+                                selectInput('municipality_input', 'Municipality',
+                                            c('Vancouver' = 'van', 'UBC' = 'ubc'),
+                                            selected = 'ubc')),
+                         column(8, 
+                                tabsetPanel(
+                                    tabPanel("Price"),
+                                    tabPanel("Income"),
+                                    tabPanel("Gap")
+                                )),
+                         column(2)
+                         ),
+                     fluidRow(
+                         column(2,
+                                h3(class = 'text-center', 'Here goes the second dropdown'),
+                                selectInput('social_input', 'Socio-Demographic Variable',
+                                            c('Age' = 'age', 'Income' = 'income'),
+                                            selected = 'income')),
+                         column(4, h3(class = 'text-center', 'Here goes the Vancouver plot')),
+                         column(4, h3(class = 'text-center', 'Here goes the neighbourhood plot')),
+                         column(2)
+                     ),
+                     fluidRow(
+                         column(2),
+                         column(8, h3(class = 'text-center', 'Here goes the table')),
+                         column(2)
+                     )
+                    )
+                 ),
+        tabPanel('About', icon = icon('info-circle'))
+    )
+)
