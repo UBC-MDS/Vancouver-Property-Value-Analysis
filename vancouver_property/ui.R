d9c0cbdc7697e060fc6637f1a2f5b48fe9d6d810
Ian Flores Siaca <iflores.siaca@gmail.com>
2019-01-18 17:46:31 -0800

    local

--- vancouver_property/ui.R
+++ vancouver_property/ui.R
@@ -10,21 +10,21 @@ shinyUI(
                          h1(class = 'text-center', 'Vancouver Property Analysis App'),
                          column(2, 
                                 h3(class = 'text-center', 'Here goes one dropdown'),
-                                selectInput('municipality_input', 'Municipality',
+                                selectInput('municipality_input', 'Neighbourhood',
                                             c('Vancouver' = 'van', 'UBC' = 'ubc'),
                                             selected = 'ubc')),
                          column(8, 
                                 tabsetPanel(
-                                    tabPanel("Price"),
-                                    tabPanel("Income"),
-                                    tabPanel("Gap")
+                                    tabPanel("Property Values"),
+                                    tabPanel("Incomes"),
+                                    tabPanel("Affordability Gap")
                                 )),
                          column(2)
                          ),
                      fluidRow(
                          column(2,
                                 h3(class = 'text-center', 'Here goes the second dropdown'),
-                                selectInput('social_input', 'Socio-Demographic Variable',
+                                selectInput('social_input', 'Socio-Economic Indicator',
                                             c('Age' = 'age', 'Income' = 'income'),
                                             selected = 'income')),
                          column(4, h3(class = 'text-center', 'Here goes the Vancouver plot')),
